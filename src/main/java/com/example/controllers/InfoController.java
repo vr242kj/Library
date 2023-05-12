2db97e4d4ac1e406fdf582475da26612a301791b
Vitaliy Replyuk <vitaliy.replyuk@student.tuke.sk>
2023-05-12 18:22:32 +0100

    refactor first controller

---
+++ src/main/java/com/example/controllers/InfoController.java
@@ -0,0 +1,28 @@
+package com.example.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1")
+public class InfoController {
+
+    @GetMapping("/welcome")
+    public ResponseEntity<Map<String, String>> getWelcomeInfo() {
+
+        var info = Map.of("message", "Welcome to the library!",
+                "currentDate", DateTimeFormatter.ofPattern("dd.MM.yyyy").format(LocalDateTime.now()));
+
+        return new ResponseEntity<>(info, HttpStatus.OK);
+    }
+}
