9de6990ff13a08279d6774bf50ce70890eed12f8
Vitaliy Replyuk <vitaliy.replyuk@student.tuke.sk>
2023-06-22 00:23:59 +0100

    add setUp and tearDown for data in BookServiceIntegrationTest

--- src/test/java/com/example/service/BookServiceIntegrationTest.java
+++ src/test/java/com/example/service/BookServiceIntegrationTest.java
@@ -1,25 +1,45 @@
 package com.example.service;
 
 import com.example.entity.Book;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.jdbc.Sql;
 
 import java.util.List;
 
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.BEFORE_TEST_METHOD;
-import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 @SpringBootTest
+@AutoConfigureTestDatabase
 @ConfigurationPropertiesScan("src/test/resources/application-test.yml")
-@Sql(value = "classpath:schema-test.sql", executionPhase = BEFORE_TEST_METHOD)
+@Sql(value = "classpath:schema.sql", executionPhase = BEFORE_TEST_METHOD)
 public class BookServiceIntegrationTest {
 
     @Autowired
     private BookService bookService;
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @BeforeEach
+    public void setUp() {
+        jdbcTemplate.execute("INSERT INTO book (name, author) VALUES ('In Search of Lost Time', 'Marcel Proust')");
+        jdbcTemplate.execute("INSERT INTO book (name, author) VALUES ('Ulysses', 'James Joyce')");
+        jdbcTemplate.execute("INSERT INTO book (name, author) VALUES ('Don Quixote', 'Miguel de Cervantes')");
+    }
+
+    @AfterEach
+    public void tearDown() {
+        jdbcTemplate.execute("DROP TABLE book");
+    }
 
     @Test
     public void testFindAllBooks() {
