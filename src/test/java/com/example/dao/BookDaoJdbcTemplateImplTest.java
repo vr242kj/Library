61f48769e6f4cb666b103b6076f96cfe7dee4114
Vitaliy Replyuk <vitaliy.replyuk@student.tuke.sk>
2023-07-03 18:27:57 +0100

    add BookDaoJdbcTemplateImplTest

--- src/test/java/com/example/dao/BookDaoJdbcTemplateImplTest.java
+++ src/test/java/com/example/dao/BookDaoJdbcTemplateImplTest.java
@@ -1,14 +1,15 @@
 package com.example.dao;
 
 import com.example.entity.Book;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.jdbc.Sql;
 
 import java.util.Optional;
@@ -20,14 +21,16 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.BEFORE_TEST_METHOD;
 
-@SpringBootTest
+@JdbcTest
 @AutoConfigureTestDatabase
 @Sql(value = "classpath:schema.sql", executionPhase = BEFORE_TEST_METHOD)
-class BookDaoJdbcImplTest {
+class BookDaoJdbcTemplateImplTest {
+    private static BookDaoJdbcTemplateImpl bookDaoJdbcImpl;
 
-    @Autowired
-    @Qualifier("bookDaoJdbcImpl")
-    private BookDao bookDaoJdbcImpl;
+    @BeforeAll
+    public static void setUp(@Autowired JdbcTemplate jdbcTemplate) {
+        bookDaoJdbcImpl = new BookDaoJdbcTemplateImpl(jdbcTemplate);
+    }
 
     @ParameterizedTest(name = "{index} ==> {2}")
     @DisplayName("Should throw DaoException when book name or/and author is null")
@@ -41,7 +44,7 @@ class BookDaoJdbcImplTest {
                 "Expected DAOException to throw, but it didn't"
         );
 
-        assertTrue(thrown.getMessage().contains("Database error during saving new book!" + "\nError details: "));
+        assertTrue(thrown.getMessage().contains("Failed to save new book"));
     }
 
     @Test
