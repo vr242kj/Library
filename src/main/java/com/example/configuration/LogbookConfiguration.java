e33ee8adcae7171037978f890c6e4d0acba2dc2d
Vitaliy Replyuk <vitaliy.replyuk@student.tuke.sk>
2023-07-14 15:39:45 +0100

    fix logbook logging

---
+++ src/main/java/com/example/configuration/LogbookConfiguration.java
@@ -0,0 +1,18 @@
+package com.example.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LogbookConfiguration {
+
+//    @Bean
+//    public Logbook logbook() {
+//        return Logbook.builder()
+//                .condition(exclude(
+//                        requestTo("**/swagger-ui/**"),
+//                        requestTo("**/api-docs/**")))
+//                .build();
+//    }
+
+}
+
